ee4d11552103690cd8393bc705e4780f65971bd5
郝祥聪 <1192484280@qq.com>
2018-06-06 17:16:30 +0800

    1

--- FreedomHeader/ViewController.m
+++ FreedomHeader/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "HeaderView.h"
 #import "Masonry.h"
+#import "UIView+Toast.h"
 
 @interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
 
@@ -100,16 +101,20 @@
     [self.tableView layoutIfNeeded];
     
     
+    [self.view makeToastActivity:CSToastPositionCenter];
+    
     //模拟请求数据
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         [self.headerView setDes:@"过放荡不羁的生活，容易得像顺水推舟，但是要结识良朋益友，却难如登天。 —— 巴尔扎克"];
         
+        //舒服的更新frame
         [self.tableView beginUpdates];
         [self.tableView setTableHeaderView:self.headerView];
         [self.tableView layoutIfNeeded];
         [self.tableView endUpdates];
         
+        [self.view hideToastActivity];
     });
 }
 
